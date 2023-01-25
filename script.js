9033686374707f65e3846b9d1e0995252c844a54
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-24 21:02:27 -0300

    erro agits

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const buttonOne = document.querySelector(".cards .card");
+const buttonOne = document.querySelector(".modalbut button ");
 const modalOne = document.querySelector("dialog");
+const buttonTwo = document.querySelector("dialog button");
+
+buttonOne.onclick = function () {
+  modalOne.showModal();
+};
+
+buttonTwo.onclick = function () {
+  modalOne.close();
+};
