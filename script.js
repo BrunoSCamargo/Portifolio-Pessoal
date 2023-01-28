db9b746a48328e128af15624d46cdbb640e5c107
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-28 20:53:07 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const controls = document.querySelectorAll(".control");
-const controls1 = document.querySelectorAll(".control1");
 let currentItem = 0;
-let currentIte = 0;
 const items = document.querySelectorAll(".item");
 const maxItems = items.length;
-const items1 = document.querySelectorAll(".item1");
-const maxItem = items1.length;
 
 controls.forEach((control) => {
   control.addEventListener("click", (e) => {
@@ -33,34 +29,5 @@ controls.forEach((control) => {
     });
 
     items[currentItem].classList.add("current-item");
-  });
-});
-
-controls1.forEach((control) => {
-  control.addEventListener("click", (e) => {
-    isLeft = e.target.classList.contains("arrow-left");
-
-    if (isLeft) {
-      currentIte -= 1;
-    } else {
-      currentIte += 1;
-    }
-
-    if (currentIte >= maxItem) {
-      currentIte = 0;
-    }
-
-    if (currentIte < 0) {
-      currentIte = maxItem - 1;
-    }
-
-    items1.forEach((item) => item.classList.remove("current-item"));
-
-    items1[currentIte].scrollIntoView({
-      behavior: "smooth",
-      inline: "center",
-    });
-
-    items1[currentIte].classList.add("current-item");
   });
 });
